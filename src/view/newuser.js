e91c292c5f4ff5e10a7c4d71fc2f2d85cfa0deb9
Mara Mulato <maramulato@MacBook-Mara.local>
2020-04-14 17:05:37 -0500

    new user sign up

--- src/view/newuser.js
+++ src/view/newuser.js
@@ -1,32 +1,47 @@
-
 export default () => {
   const viewNewUser = `
-  <form id="sign-up">
-  <input type="email" placeholder="Email" id="emailText"> </input>
-  <input type="password" placeholder="Password" id="passwordText"> </input>
-  <button type="submit" class="btn" id="loginBtn"> Sign Up </button>
-  </form>
-  `;
-  // nodes
-  const divElement = document.createElement('div')
-  divElement.innerHTML = viewNewUser
+  <div class = "gridContainer" id = "signupForm">
+        <div class = "about">
+            <h3> Sign Up!</h3>
+        </div>
+        <div class = "formContainer">
+            <form class = "inputForm" id = "form">
+                <input type="file" id="img" name="img" accept="image/*">
+                <input class = "registerInput" id = "signupUser" type = "text" placeholder = "Username" required>
+                <input class = "registerInput" id = "signupEmail" type = "email" placeholder = "Email" required> 
+                <input class = "registerInput" id = "signupPassword" type = "password" placeholder = "Password" required> 
+                <button class= "btn" id = "signup"> Sign up! </button>
+            </form>
+        </div> 
+        <div class = "createAccout">
+            <p> Sign up with </p>
+        </div>
+        <div class = "fbYG">
+            <button> fb </button>
+            <button> google </button>
+        </div>   
+  </div>`;
 
-  // the sign up form
-  const signupForm = divElement.querySelector('#sign-up');
-  signupForm.addEventListener('submit', (e) => {
-   e.preventDefault(); 
+  // nodes Render
+  const divElement = document.createElement('div');
+  divElement.innerHTML = viewNewUser;
 
-   // get user info
-   const email = signupForm['emailText'].value;
-   const password = signupForm['passwordText'].value;
-   const auth = firebase.auth();
+  // Nodes from DOM elements inside the form
+  const emailText = divElement.querySelector('#signupEmail');
+  const passwordText = divElement.querySelector('#signupPassword');
+  const signupBtn = divElement.querySelector('#signup');
 
-   // sign up the user
-   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-     console.log(cred.user)
-   }) 
-  })
+  // signup Event
+  signupBtn.addEventListener('click', (e) => {
+    const email = emailText.value;
+    const pass = passwordText.value;
+    const auth = firebase.auth();
 
-
-  return divElement
-}
+    // sign up User
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.then((cred) => {
+      console.log(cred);
+    });
+  });
+  return divElement;
+};
