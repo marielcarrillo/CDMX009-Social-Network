d36dd0ecfabec8eb88db8d74fac252cf5f2f2581
Mara Mulato <maramulato@MacBook-Mara.local>
2020-04-15 09:58:38 -0500

    logout option

--- src/view/login.js
+++ src/view/login.js
@@ -3,9 +3,10 @@ import { changeView } from '../view-controler/router.js';
 export default () => {
   const viewLogin = `
     <form>
-    <input type='email' placeholder='Email' id="emailText"> </input>
-    <input type='password' placeholder='Password' id="passwordText"> </input>
-    <button class='btn' id='loginBtn'> puchame </button>
+    <input type="email" placeholder="Email" id="emailText"> </input>
+    <input type="password" placeholder="Password" id="passwordText"> </input>
+    <button class="btn" id="loginBtn"> Login </button>
+    <button class="btn" id="logoutBtn"> Logout </button>
     </form>
   `;
 
@@ -17,17 +18,27 @@ export default () => {
   const emailText = divElement.querySelector('#emailText');
   const passwordText = divElement.querySelector('#passwordText');
   const loginBtn = divElement.querySelector('#loginBtn');
+  const auth = firebase.auth();
 
   // login event
   loginBtn.addEventListener('click', (e) => {
     const email = emailText.value;
     const pass = passwordText.value;
-    const auth = firebase.auth();
 
     // sign in with firebase functions
     const promise = auth.signInWithEmailAndPassword(email, pass);
     promise.then(e => changeView('#/home'));
     promise.catch(e => console.log(alert(e.message)));
+  });
+
+  // logout
+  const logout = divElement.querySelector('#logoutBtn');
+  logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+      console.log('user signed out');
+      alert('Come back soon!');
+    });
   });
 
   return divElement;
--- src/view/newuser.js
+++ src/view/newuser.js
@@ -1,3 +1,5 @@
+import { changeView } from '../view-controler/router.js';
+
 export default () => {
   const viewNewUser = `
   <div class = "gridContainer" id = "signupForm">
@@ -12,35 +14,59 @@ export default () => {
                 <input class = "registerInput" id = "signupPassword" type = "password" placeholder = "Password" required> 
                 <button class= "btn" id = "signup"> Sign up! </button>
             </form>
-        </div> 
+        </div>
         <div class = "createAccout">
             <p> Sign up with </p>
         </div>
         <div class = "fbYG">
-            <button> fb </button>
-            <button> google </button>
-        </div>   
+            <button id = "fb"> fb </button>
+            <button id = "google"> google </button>
+        </div>
   </div>`;
 
   // nodes Render
   const divElement = document.createElement('div');
   divElement.innerHTML = viewNewUser;
 
-  // Nodes from DOM elements inside the form
+  // Nodes from DOM elements
   const emailText = divElement.querySelector('#signupEmail');
   const passwordText = divElement.querySelector('#signupPassword');
   const signupBtn = divElement.querySelector('#signup');
+  const fbBtn = divElement.querySelector('#fb');
+  const gBtn = divElement.querySelector('#google');
+  const auth = firebase.auth();
 
-  // signup Event
+  // signup with Email
   signupBtn.addEventListener('click', (e) => {
     const email = emailText.value;
     const pass = passwordText.value;
-    const auth = firebase.auth();
+    
 
-    // sign up User
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.then((cred) => {
-      console.log(cred);
+    promise.then(e => changeView('#/login'));
+  });
+
+  // facebook sign up
+  fbBtn.addEventListener('click', () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    const promise = auth.signInWithPopup(provider);
+
+    promise.then(e => changeView('#/home'));
+    promise.catch((error) => {
+      alert('no salió :(');
+      console.log(error);
+    });
+  });
+
+  // google sign up
+  gBtn.addEventListener('click', () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const promise = auth.signInWithPopup(provider);
+
+    promise.then(e => changeView('#/home'));
+    promise.catch((error) => {
+      alert('no salió :(');
+      console.log(error);
     });
   });
   return divElement;
