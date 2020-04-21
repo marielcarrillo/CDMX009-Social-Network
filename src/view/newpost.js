9ab6f6def754e2b89aedcaa318596d83df17e089
Mara Mulato <maramulato@MacBook-Mara.local>
2020-04-21 13:51:59 -0500

    previsualiza img antes de subirla

--- src/view/newpost.js
+++ src/view/newpost.js
@@ -1,7 +1,10 @@
 export default () => {
-// var globales
-// var db = firebase.firestore();
-  const storage = firebase.storage();
+  // firebase
+  let db = firebase.firestore();
+  let storage = firebase.storage();
+  let docRef = db.collection('posts');
+
+  // var globales
   let url;
 
   const viewNewPost = `<div class = "gridContainer">
@@ -11,7 +14,6 @@ export default () => {
           <input id="registerDescription" type = "text" placeholder = "Descripción" required>
           <input id="registerLocation" type = "text" placeholder = "Ubicación" required>
           <button class="btn" id = "btnShare"> Share </button>
-          <button class="btn" id="btnLoad"> Load </button>
 
          <div>
          <img src="" id="newPost" width="300px" height="200px">
@@ -32,14 +34,26 @@ export default () => {
 
   // get the div elements
   const imgUpload = divElement.querySelector('#imgUpload');
-  const loadImg = divElement.querySelector('#btnLoad');
-  // const registerDescription = divElement.querySelector('#registerDescription')
-  // const registerLocation = divElement.querySelector('#registerLocation')
+  const shareImg = divElement.querySelector('#btnShare');
+  const registerDescription = divElement.querySelector('#registerDescription');
+  const registerLocation = divElement.querySelector('#registerLocation');
 
+  // Display the image
+  imgUpload.onchange = function (e) {
+    // FileReader permite leer files o blob del lado cliente de manera asíncrona
+    let reader = new FileReader();
+    // Lee el archivo y lo manda a FileReader
+    reader.readAsDataURL(e.target.files[0]);
+    // Cuando esté listo, ejecuta el código
+    reader.onload = function () {
+      const image = divElement.querySelector('#newPost');
+      image.src = reader.result;
+      image.innerHTML = '';
+    };
+  };
   // func + listener for uploading the image to storage
-
   imgUpload.addEventListener('change', (e) => {
-    // get file
+  // get file
     const image = divElement.querySelector('#imgUpload').files[0];
     const imageName = image.name;
 
@@ -55,50 +69,18 @@ export default () => {
       });
   });
 
-  // Get the url into a blob. Documentación de firebase
-  function load() {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = (e) => {
-      const blob = xhr.response;
-    };
-    xhr.open('GET', url);
-    xhr.send();
+  // sending the info to firestore
+  shareImg.addEventListener('click', (e) => {
+    const descr = registerDescription.value;
+    const loc = registerLocation.value;
 
-    // Insert into an <img> element:
-    const img = divElement.querySelector('#newPost');
-    img.src = url;
-  }
-
-  loadImg.addEventListener('click', load);
-
-
-  // func + listener to upload the info to firestore
-  /* loadImg.addEventListener('click', e =>{
-    const docRef = db.doc("posts/feed")
-    const descriptionValue = registerDescription.value
-    const locationValue = registerLocation.value
-    docRef.set({
-      description: descriptionValue,
-      location: locationValue
-    }).then((e) => {
-      console.log(':) OK!');
-    }).catch((e) => {
-      console.log(':( notOk!');
-    });
+    docRef.add({
+      postimg: url,
+      description: descr,
+      location: loc,
+      date: firebase.firestore.Timestamp.fromDate(new Date()),
+    }).then(e => console.log('ok'));
   });
-  /*func + listener to upload the info to realtime database
-  loadImg.addEventListener('click', e =>{
-    //create database reference
-
-    const databaseRef = database.ref('post-data')
-    const data = {
-      description: registerDescription.value,
-      location: registerLocation.value
-    }
-    databaseRef.push(data)
-  }) */
-
 
   return divElement;
 };
